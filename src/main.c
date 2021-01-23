afe869d7e762f1e465fe1065eebca79623265d96
Adin W-T <adinrwt@gmail.com>
2021-01-23 01:41:06 -0500

    Basic scrolling unlocked

--- src/main.c
+++ src/main.c
@@ -7,6 +7,9 @@
 
 #define IN_RANGE(x, min, max) ((x >= min) && (x <= max))
 
+#define LINE_LENGTH 16
+#define DEFAULT_FONT "Monospace Normal 12"
+
 #define HEX_BUFFER_LENGTH 48
 #define HEX_BUFFER_OFFSET(x) (x * 2 + (x - 1))
 
@@ -24,6 +27,9 @@ struct _ProgramState {
     GtkWidget *offsetBox;
     GtkWidget *hexBox;
     GtkWidget *asciiBox;
+    GtkWidget *scrollBar;
+
+    GtkAdjustment *scrollAdj;
 
     PangoFontDescription *fontDesc;
     uint fontWidth;
@@ -49,6 +55,15 @@ static ProgramState state = {0};
 
 void closeCurrentFile(bool titleUpdateNeeded);
 void updateTitle();
+void updateSizeRequests();
+
+int jceil(double value) {
+    if(value > (int) value) {
+        return (int) value + 1;
+    }
+
+    return (int) value;
+}
 
 void shutdownAndCleanup() {
     // TODO(Adin): Close files and do cleanup here
@@ -78,6 +93,8 @@ void closeCurrentFile(bool titleUpdateNeeded) {
     state.fileLength = 0;
     state.fileNumLines = 0;
 
+    updateSizeRequests();
+
     if(state.fileWidgetsBox) {
         gtk_widget_queue_draw(state.fileWidgetsBox);
     }
@@ -98,6 +115,10 @@ void openFile(char *filename) {
     state.fileBuffer = malloc(state.fileLength + 1);
     memset(state.fileBuffer, 0, state.fileLength + 1);
     fread(state.fileBuffer, 1, state.fileLength, state.file);
+
+    state.fileNumLines = jceil((float) state.fileLength / (float) LINE_LENGTH);
+
+    updateSizeRequests();
 
     if(state.fileWidgetsBox) {
         gtk_widget_queue_draw(state.fileWidgetsBox);
@@ -145,16 +166,28 @@ uint getFontWidth(GtkWidget *widget, PangoFontDescription *fontDesc) {
 }
 
 void updateSizeRequests() {
+    int height = -1;
+
+    if(state.file) {
+        height = MIN(10, state.fileNumLines) * state.fontHeight;
+    }
+
+    if(state.fileWidgetsBox) {
+        gtk_widget_set_size_request(state.fileWidgetsBox, -1, height);
+    }
+
     if(state.offsetBox) {
-        gtk_widget_set_size_request(state.offsetBox, 8 * state.fontWidth + 2 * TEXT_MARGIN_PX, -1);
+        gtk_widget_set_size_request(state.offsetBox, 8 * state.fontWidth + 2 * TEXT_MARGIN_PX, height);
         // printf("OffsetBox: %d\n", state.offsetBox, 8 * state.fontWidth + 2 * TEXT_MARGIN_PX);
     }
+
     if(state.hexBox) {
-        gtk_widget_set_size_request(state.hexBox, (HEX_BUFFER_LENGTH - 1) * state.fontWidth + 2 * TEXT_MARGIN_PX, -1);
+        gtk_widget_set_size_request(state.hexBox, (HEX_BUFFER_LENGTH - 1) * state.fontWidth + 2 * TEXT_MARGIN_PX, height);
         // printf("HexBox: %d\n", state.offsetBox, (HEX_BUFFER_LENGTH - 1) * state.fontWidth + 2 * TEXT_MARGIN_PX);
     }
+
     if(state.asciiBox) {
-        gtk_widget_set_size_request(state.asciiBox, (ASCII_BUFFER_LENGTH - 1) * state.fontWidth + 2 * TEXT_MARGIN_PX, -1);
+        gtk_widget_set_size_request(state.asciiBox, (ASCII_BUFFER_LENGTH - 1) * state.fontWidth + 2 * TEXT_MARGIN_PX, height);
         // printf("AsciiBox: %d\n", (ASCII_BUFFER_LENGTH - 1) * state.fontWidth + 2 * TEXT_MARGIN_PX);
     }
 }
@@ -186,7 +219,10 @@ void updateFont(PangoFontDescription *newDesc) {
     if(state.fileWidgetsBox) {
         gtk_widget_queue_draw(state.fileWidgetsBox);
     }
+}
 
+void onScrollEvent(GtkWidget *widget, GdkEvent *event) {
+    gtk_widget_event(state.scrollBar, event);
 }
 
 void fontMenuAction(GtkMenuItem *menuItem) {
@@ -204,10 +240,6 @@ void fontMenuAction(GtkMenuItem *menuItem) {
     }
 
     gtk_widget_destroy(dialog);
-}
-
-void resizeMenuAction(GtkMenuItem *menuItem) {
-    gtk_widget_set_size_request(state.fileWidgetsBox, 300, 100);
 }
 
 void updateTitle() {
@@ -229,7 +261,7 @@ void updateTitle() {
     }
 }
 
-void updateSize(GtkWidget *widget, GdkRectangle *newRectangle) {
+void onUpdateSize(GtkWidget *widget, GdkRectangle *newRectangle) {
     if(newRectangle) {
         state.widgetHeight = newRectangle->height; 
     }
@@ -240,13 +272,18 @@ void updateSize(GtkWidget *widget, GdkRectangle *newRectangle) {
             state.numLines++;
         }
     }
+
+    if(state.scrollAdj) {
+        uint value = gtk_adjustment_get_value(state.scrollAdj);
+        gtk_adjustment_configure(state.scrollAdj, value, 0, state.fileNumLines, 1, 1, state.numLines);
+    }
 }
 
 void fillHexBuffer(ulong offset) {
     // TODO(Adin): Update for when ending lines in a file are less than a full line
     // TODO(Adin): Update when lines are resizable
 
-    for(long i = 0; i < 16; i++) {
+    for(long i = 0; i < MIN(LINE_LENGTH, state.fileLength - offset); i++) {
         if(i == 0) {
             snprintf(state.hexLineBuffer, HEX_BUFFER_LENGTH, "%02X", state.fileBuffer[offset + i]);
         }
@@ -261,7 +298,7 @@ void fillAsciiBuffer(ulong offset) {
     // TODO(Adin): Update when lines are resizable
     char current = 0;
 
-    for(long i = 0; i < 16; i++) {
+    for(long i = 0; i < MIN(LINE_LENGTH, state.fileLength - offset); i++) {
         if(IN_RANGE(state.fileBuffer[offset + i], 0x20, 0x7E)) {
             current = state.fileBuffer[offset + i];
         }
@@ -299,8 +336,10 @@ gboolean renderOffsetBox(GtkWidget *widget, cairo_t *cr) {
     gdk_cairo_set_source_rgba(cr, &fgColor);
     pango_layout_set_font_description(pangoLayout, state.fontDesc);
 
-    for(int i = 0; i < state.numLines; i++) {
-        snprintf(buffer, 9, "%08X", i * 0x10);
+    uint linesToDraw = MIN(state.numLines, state.fileNumLines); // TODO(Adin): Update for scrolling
+    uint adjValue = gtk_adjustment_get_value(state.scrollAdj);
+    for(int i = 0; i < linesToDraw; i++) {
+        snprintf(buffer, 9, "%08X", (adjValue * LINE_LENGTH) + i * LINE_LENGTH);
         pango_layout_set_text(pangoLayout, buffer, 10);
 
         cairo_move_to(cr, TEXT_MARGIN_PX, i * state.fontHeight);
@@ -340,8 +379,10 @@ gboolean renderHexBox(GtkWidget *widget, cairo_t *cr) {
     gdk_cairo_set_source_rgba(cr, &fgColor);
     pango_layout_set_font_description(pangoLayout, state.fontDesc);
 
-    for(int i = 0; i < state.numLines; i++) {
-        fillHexBuffer(i * 0x10);
+    uint linesToDraw = MIN(state.numLines, state.fileNumLines); // TODO(Adin): Update for scrolling
+    uint adjValue = gtk_adjustment_get_value(state.scrollAdj);
+    for(int i = 0; i < linesToDraw; i++) {
+        fillHexBuffer((adjValue * LINE_LENGTH) + i * LINE_LENGTH);
         pango_layout_set_text(pangoLayout, state.hexLineBuffer, -1);
 
         cairo_move_to(cr, TEXT_MARGIN_PX, i * state.fontHeight);
@@ -377,8 +418,10 @@ gboolean renderAsciiBox(GtkWidget *widget, cairo_t *cr) {
     gdk_cairo_set_source_rgba(cr, &fgColor);
     pango_layout_set_font_description(pangoLayout, state.fontDesc);
 
-    for(int i = 0; i < state.numLines; i++) {
-        fillAsciiBuffer(i * 0x10);
+    uint linesToDraw = MIN(state.numLines, state.fileNumLines); // TODO(Adin): Update for scrolling
+    uint adjValue = gtk_adjustment_get_value(state.scrollAdj);
+    for(int i = 0; i < linesToDraw; i++) {
+        fillAsciiBuffer((adjValue * LINE_LENGTH) + i * LINE_LENGTH);
         pango_layout_set_text(pangoLayout, state.asciiLineBuffer, -1);
 
         cairo_move_to(cr, TEXT_MARGIN_PX, i * state.fontHeight);
@@ -390,6 +433,13 @@ gboolean renderAsciiBox(GtkWidget *widget, cairo_t *cr) {
     return FALSE;
 }
 
+void onAdjValueChanged(GtkAdjustment *adj) {
+    printf("%f\n", gtk_adjustment_get_value(adj));
+    if(state.fileWidgetsBox) {
+        gtk_widget_queue_draw(state.fileWidgetsBox);
+    }
+}
+
 GtkWidget *buildMenu() {
     GtkWidget *menubar =     NULL;
 
@@ -399,7 +449,6 @@ GtkWidget *buildMenu() {
     GtkWidget *openMenuI =   NULL;
     GtkWidget *closeMenuI =  NULL;
     GtkWidget *fontMenuI =   NULL;
-    GtkWidget *resizeMenuI = NULL;
 
     menubar =     gtk_menu_bar_new();
     fileMenu =    gtk_menu_new();
@@ -408,12 +457,10 @@ GtkWidget *buildMenu() {
     openMenuI =   gtk_menu_item_new_with_label("Open");
     closeMenuI =  gtk_menu_item_new_with_label("Close");
     fontMenuI =   gtk_menu_item_new_with_label("Font");
-    resizeMenuI = gtk_menu_item_new_with_label("Resize");
 
     g_signal_connect(G_OBJECT(openMenuI),   "activate", G_CALLBACK(openMenuAction),   NULL);
     g_signal_connect(G_OBJECT(closeMenuI),  "activate", G_CALLBACK(closeCurrentFile), NULL);
     g_signal_connect(G_OBJECT(fontMenuI),   "activate", G_CALLBACK(fontMenuAction),   NULL);
-    g_signal_connect(G_OBJECT(resizeMenuI), "activate", G_CALLBACK(resizeMenuAction), NULL);
 
     gtk_menu_shell_append(GTK_MENU_SHELL(menubar), fileMenuI);
     gtk_menu_item_set_submenu(GTK_MENU_ITEM(fileMenuI), fileMenu);
@@ -421,7 +468,6 @@ GtkWidget *buildMenu() {
     gtk_menu_shell_append(GTK_MENU_SHELL(fileMenu), openMenuI);
     gtk_menu_shell_append(GTK_MENU_SHELL(fileMenu), closeMenuI);
     gtk_menu_shell_append(GTK_MENU_SHELL(fileMenu), fontMenuI);
-    gtk_menu_shell_append(GTK_MENU_SHELL(fileMenu), resizeMenuI);
 
     return menubar;
 }
@@ -444,7 +490,7 @@ int main(int argc, char **argv) {
     gtk_window_set_icon_from_file(GTK_WINDOW(state.window), "jafhe.svg", NULL); // Does this even do anything?
     g_signal_connect(G_OBJECT(state.window), "destroy", G_CALLBACK(shutdownAndCleanup), NULL);
 
-    defaultFontDesc = pango_font_description_from_string("Monospace Normal 12");
+    defaultFontDesc = pango_font_description_from_string(DEFAULT_FONT);
     updateFont(defaultFontDesc);
 
     vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
@@ -461,24 +507,34 @@ int main(int argc, char **argv) {
     state.hexBox = gtk_drawing_area_new();
     hexStyleContext = gtk_widget_get_style_context(state.hexBox);
     gtk_style_context_add_class(hexStyleContext, GTK_STYLE_CLASS_VIEW);
-    g_signal_connect(state.hexBox, "size-allocate", G_CALLBACK(updateSize), NULL);
+    gtk_widget_set_events(state.hexBox, GDK_SCROLL_MASK);
+    g_signal_connect(state.hexBox, "size-allocate", G_CALLBACK(onUpdateSize), NULL);
     g_signal_connect(state.hexBox, "draw", G_CALLBACK(renderHexBox), NULL);
+    g_signal_connect(state.hexBox, "scroll-event", G_CALLBACK(onScrollEvent), NULL);
 
     state.asciiBox = gtk_drawing_area_new();
     asciiStyleContext = gtk_widget_get_style_context(state.asciiBox);
     gtk_style_context_add_class(asciiStyleContext, GTK_STYLE_CLASS_VIEW);
+    gtk_widget_set_events(state.asciiBox, GDK_SCROLL_MASK);
     g_signal_connect(state.asciiBox, "draw", G_CALLBACK(renderAsciiBox), NULL);
+    g_signal_connect(state.asciiBox, "scroll-event", G_CALLBACK(onScrollEvent), NULL);
+
+    state.scrollAdj = gtk_adjustment_new(0.0, 0.0, 100.0, 1.0, 1.0, 20.0); // TODO(Adin): Change
+    g_signal_connect(state.scrollAdj, "value-changed", G_CALLBACK(onAdjValueChanged), NULL);
+    state.scrollBar = gtk_scrollbar_new(GTK_ORIENTATION_VERTICAL, state.scrollAdj);
 
     updateSizeRequests();
 
     gtk_box_pack_start(GTK_BOX(state.fileWidgetsBox), state.offsetBox, FALSE, FALSE, 0);
     gtk_box_pack_start(GTK_BOX(state.fileWidgetsBox), state.hexBox, FALSE, FALSE, 0);
     gtk_box_pack_start(GTK_BOX(state.fileWidgetsBox), state.asciiBox, FALSE, FALSE, 0);
+    gtk_box_pack_end(GTK_BOX(state.fileWidgetsBox), state.scrollBar, FALSE, FALSE, 0);
 
     gtk_box_pack_start(GTK_BOX(vbox), menubar, FALSE, FALSE, 0);
     gtk_box_pack_start(GTK_BOX(vbox), state.fileWidgetsBox, TRUE, TRUE, 0);
 
     gtk_widget_show_all(state.window);
+    // gtk_widget_hide(state.scrollBar);
 
     gtk_main();
 
